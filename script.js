222a0bf2369274e9e00e16ab3dee407aacf819b8
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-10-27 17:20:40 +0500

    Update and rename index.html to script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Document</title>
+
+<link rel="stylesheet" href="style.css">
+
+<script>
+
+var x= 2;
+
+var y= 4;
+
+
+if(x == y){
+
+alert("SUBSCRIBE US!");
+
+} else{
+
+alert("Allow our Notification Form");
+
+}
+
+</script>
+
+</head>
+
+
+
+<body>
+
+</body>
+
+</html>
+
+//confirm box in js -------
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Document</title>
+
+<link rel="stylesheet" href="style.css">
+
+<script>
+
+var a= confirm("do you like our service");
+
+if(a) {
+
+alert("Thankyou for your feedback");
+
+}else{
+
+alert("we are sorry for our bad service");
+
+}
+
+</script>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+
